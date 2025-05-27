1ef8a4671f44a3d46de7ecc4e233991177d0e862
reddred <anatoliivysotskyi1@gmail.com>
2025-05-27 18:16:36 +0200

    add functionality for lower case text

--- cesar.rb
+++ cesar.rb
@@ -9,10 +9,18 @@ output = ''
 text.each_char do | str |
   if str =~ /[A-Z]/
     shifted = (str.ord - 65 + shifts) % 26
-    if shifted < 26
+    if shifted < 0
       shifted += 26
     end
     output += (shifted + 65 ).chr
+  elsif str =~ /[a-z]/
+    shifted = (str.ord - 97 + shifts) % 26
+    if shifted < 0
+      shifted += 26
+    end
+    output += (shifted + 97).chr
+  else
+    output += str
   end
 end
 
